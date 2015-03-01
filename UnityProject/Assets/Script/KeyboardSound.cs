465dbbbc9fc3ebcefa0e3c30acea1542cec2e1bc
kawannam <kendrasemail@shaw.ca>
2015-03-01 16:50:35 -0700

    Piano Basic stuff
    two new functions

---
+++ UnityProject/Assets/Script/KeyboardSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardSound : MonoBehaviour { 
+
+	public AudioSource source;
+
+	void Update() {
+		if (Input.GetKeyDown("q"))
+			source.Play();
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
