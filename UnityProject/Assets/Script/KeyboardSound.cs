4b12836088290c597b2c58e313deab30ec85e921
kawannam <kendrasemail@shaw.ca>
2015-03-01 18:55:11 -0700

    Working Piano (Sort of)
    makes the noise for the white notes. Both keyboard and mouse click

--- UnityProject/Assets/Script/KeyboardSound.cs
+++ UnityProject/Assets/Script/KeyboardSound.cs
@@ -3,11 +3,32 @@ using System.Collections;
 
 public class KeyboardSound : MonoBehaviour { 
 
-	public AudioSource source;
+	public AudioSource source0;
+	public AudioSource source1;
+	public AudioSource source2;
+	public AudioSource source3;
+	public AudioSource source4;
+	public AudioSource source5;
+	public AudioSource source6;
+	public AudioSource source7;
 
 	void Update() {
-		if (Input.GetKeyDown("q"))
-			source.Play();
+		if (Input.GetKeyDown("a"))
+			source0.Play();
+		if (Input.GetKeyDown("s"))
+			source1.Play();
+		if (Input.GetKeyDown("d"))
+			source2.Play();
+		if (Input.GetKeyDown("f"))
+			source3.Play();
+		if (Input.GetKeyDown("g"))
+			source4.Play();
+		if (Input.GetKeyDown("h"))
+			source5.Play();
+		if (Input.GetKeyDown("j"))
+			source6.Play();
+		if (Input.GetKeyDown("k"))
+			source7.Play();
 		
 	}
 	// Use this for initialization
---
+++ UnityProject/Assets/Script/QSoundScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QSoundScript : MonoBehaviour {
+
+	public AudioSource source1;
+	
+	void Update() {
+		if (Input.GetKeyDown("q"))
+			source1.Play();
+		
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+}	
+
