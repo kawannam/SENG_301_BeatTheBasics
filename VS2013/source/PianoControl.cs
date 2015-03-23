48c559de481003b594a7ab979f33c1fd56e971af
mrbrian <brianyee@hotmail.com>
2015-03-22 18:28:51 -0600

    VS proj

---
+++ VS2013/source/PianoControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+public class PianoControl : IGameControl
+{
+	private IGameModel model;
+	private IGameView view;
+
+	public PianoControl()
+	{
+	}
+
+	public PianoControl(IGameModel paramModel, IGameView paramView)
+	{
+		model = paramModel;
+		view = paramView;
+	}
+	
+	public void InteractMenu(MenuAction paramAction)
+	{
+	}
+	
+	public void InteractPianoKey(PianoKey paramKey, KeyState paramState)
+	{
+		if (model != null)
+			model.InteractPianoKey(paramKey, paramState);
+	}
+
+	public void SetModel(IGameModel paramModel)
+	{
+		model = paramModel;
+	}
+	
+	public void SetView(IGameView paramView)
+	{
+		view = paramView;
+	}
+}
---
+++ VS2013/source/interfaces/IGameModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface IGameModel
+{
+	KeyState[] CurrKeyStates{ get; }
+    KeyState[] PrevKeyStates { get; }
+    AudioClip[] AudioClips { get; }
+    AudioSource[] AudioSources{ get; }
+
+	void InteractPianoKey(PianoKey paramKey, KeyState paramState);
+	void AddObserver(IGameView paramView);
+	void RemoveObserver(IGameView paramView);
+	void NotifyObservers();
+}
