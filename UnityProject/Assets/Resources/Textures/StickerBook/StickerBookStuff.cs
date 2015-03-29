4e33a16ff06128529d5e6f673b0b7080bc811204
kawannam <kendrasemail@shaw.ca>
2015-03-29 13:53:15 -0600

    StickerBook
    Sticker book done

--- UnityProject/Assets/Resources/Textures/StickerBook/StickerBookStuff.cs
+++ UnityProject/Assets/Resources/Textures/StickerBook/StickerBookStuff.cs
@@ -3,29 +3,32 @@ using UnityEngine.UI;
 using System.Collections;
 
 
-public class StickerBookStuff : MonoBehaviour {
+public class StickerBookStuff : GameModeScript{
 
 	public Image[] StickerList = new Image[9];
-	public Image[] Black_Stickers = new Image[9];
 
 	public static int[] GameProgress = new int[9];		//Checks if game has been completed 
 	// Use this for initialization
 	void Start () {
-		for ( int i = 0; i < GameProgress.Length -1; i++){
+		for ( int i = 0; i < GameProgress.Length; i++){
 			GameProgress[i] = 0;
 		}
-		//StickerList [0] = GetComponent<Image>("unicorn"); 	
 	}
 
 
 
 	// Update is called once per frame
 	void Update () {
-		if (GameProgress [0] == 0) {
-			StickerList[0].color = new Color(0,0,0,1);
-		}
-		else{
-			StickerList[0].color = new Color(1, 1, 1, 1);
+		for (int i = 0; i < StickerList.Length; i++) {
+			if (GameProgress [i] == 0) {
+				StickerList [i].color = new Color (0, 0, 0, 1);
+			} else {
+				StickerList [i].color = new Color (1, 1, 1, 1);
+			}
 		}
 	}
+	public void quit(){
+		gameManager.ChangeState (GameManagerState.Menu);
+	}
 }
+
