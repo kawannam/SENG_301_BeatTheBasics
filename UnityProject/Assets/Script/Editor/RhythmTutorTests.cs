76f7c56e3f8b6c3d36137ff4e7cd8b567c95371a
ieliopou <ieliopou@ucalgary.ca>
2015-04-02 21:37:15 -0600

    rhythm tutor test

--- UnityProject/Assets/Script/Editor/RhythmTutorTests.cs
+++ UnityProject/Assets/Script/Editor/RhythmTutorTests.cs
@@ -13,7 +13,16 @@ internal class RhythmTutorTests
 	[Category("Expected Outcome Test")]
 	public void RhythmTutor_test()
 	{
-		RhythmTutor rhyTut = new RhythmTutor();
+		GameObject t = Resources.Load<GameObject> ("Prefabs/Rhythm Object");
+		RhythmTutor rhyTut = t.GetComponent<RhythmTutor> ();
+		rhyTut.mapScore = new int[12];
+		for (int counter = 0; counter < 12; counter++) {
+			rhyTut.mapScore[counter] = 1;
+		}
+		rhyTut.SetDifficulty (Difficulty.Hard);
+		int actual = rhyTut.NumWrong ();
+		int expected = 0;
+		Assert.AreEqual (actual, expected);
 		
 	}
 
