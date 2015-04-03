631cb157a001cc2f7e2f5f6c9cbf3fd163de9ade
ykibrahi <ykibrahi@gmail.com>
2015-04-02 22:16:46 -0600

    SheetMusicNotes Test for Duration check

---
+++ UnityProject/Assets/Script/Editor/SheetTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+using UnityTest;
+
+[TestFixture]
+[Category("My Tests")]
+internal class SheetMusicNoteTest
+{
+	[Test]
+	[Category("Expected Outcome Test")]
+	public void Test_Duration()
+	{
+		SheetMusicNote sheTest = new SheetMusicNote (NoteType.Whole, PianoKey.L_C);
+
+		float expected = 1.0f;
+		float actual = sheTest.Duration;
+		Assert.AreEqual (expected, actual, "Duration is incorrect");
+	}
+}
