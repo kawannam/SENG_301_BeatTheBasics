951ad187f79e617b108dc5421d28a904557c01be
mrbrian <brianyee@hotmail.com>
2015-04-03 02:08:28 -0600

    removed 1 test

--- UnityProject/Assets/Script/Editor/PianoKeyTest.cs
+++ UnityProject/Assets/Script/Editor/PianoKeyTest.cs
@@ -33,18 +33,6 @@ internal class PianoKeyTests
 		Assert.AreEqual (expected, actual, "Mouse up not registered");
 	}
 
-	//Test pressed for Mouse Enter method
-	[Test]
-	[Category("Expected Outcome Test")]
-	public void Test_MouseEnter()
-	{
-		PianoKeyScript pKey = new PianoKeyScript ();
-		pKey.OnMouseEnter ();
-		bool expected = true;
-		bool actual = pKey.pressed;
-		Assert.AreEqual (expected, actual, "Mouse Enter not registered");
-	}
-
 	//Test pressed for Mouse exit method
 	[Test]
 	[Category("Expected Outcome Test")]
