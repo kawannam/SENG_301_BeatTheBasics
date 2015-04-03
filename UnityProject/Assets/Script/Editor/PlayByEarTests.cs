06def3d21c812686ef05190b824f80efda26ef3f
mrbrian <brianyee@hotmail.com>
2015-04-02 21:13:43 -0600

    remove duplicate tests

--- UnityProject/Assets/Script/Editor/PlayByEarTests.cs
+++ UnityProject/Assets/Script/Editor/PlayByEarTests.cs
@@ -37,37 +37,4 @@ internal class GameTests
 		actual = earObj.NUM_OF_NOTES;
 		Assert.AreEqual (expected, actual);
 	}
-
-	[Test]
-	[Category("Expected Outcome Test")]
-	public void TestSBBlank()
-	{
-		for (int i =0; i < 9; i++) {
-			
-			StickerBookScript.GameProgress [i] = 0;
-			GameObject t = Resources.Load<GameObject> ("Prefabs/StickerBookObject");
-			StickerBookScript stickerObj = t.GetComponent<StickerBookScript> ();
-			stickerObj.Update ();
-			Color checkClr = new Color (0, 0, 0, 1);
-			Color expected = checkClr;
-			Color actual = stickerObj.StickerList [i].color;
-			Assert.AreEqual (expected, actual);
-		}
-	}
-	[Test]
-	[Category("Expected Outcome Test")]
-	public void TestSBFilledIn()
-	{
-		for (int i =0; i < 9; i++) {
-			
-			StickerBookScript.GameProgress [i] = 1;
-			GameObject t = Resources.Load<GameObject> ("Prefabs/StickerBookObject");
-			StickerBookScript stickerObj = t.GetComponent<StickerBookScript> ();
-			stickerObj.Update ();
-			Color checkFull = new Color (1, 1, 1, 1);
-			Color expected = checkFull;
-			Color actual = stickerObj.StickerList [i].color;
-			Assert.AreEqual (expected, actual);
-		}
-	}
 }
